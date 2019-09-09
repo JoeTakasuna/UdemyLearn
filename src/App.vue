b7dccfa417c633dd2c7379d9502f4843ee2d32f2
JoeTakasuna <jonpili99@gmail.com>
2019-09-09 10:14:54 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+  // data () {
+  //   return {
+  //     データの置き場所
+  //   }
+  // },
+  // props: {
+  //   データの受け取り
+  // },
+  // computed: {
+  //   データの算出
+  // },
+  // methods: {
+  //   メソッドの組み込み
+  // },
+  // created: {
+  //   インスタンスが作成された後に同期的に呼ばれるフック
+  // },
+  // mounted: {
+  //   インスタンスがマウントされたちょうど後に呼ばれるフック
+  // }
+};
+</script>
