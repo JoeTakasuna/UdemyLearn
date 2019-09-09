b8341d30cb34a1d544b560a11484980a8c48deef
JoeTakasuna <jonpili99@gmail.com>
2019-09-09 21:14:07 +0900

    イベントハンドリングの実験

---
+++ src/components/Sandbox3.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .container
+    h1 Sandbox3
+    h2 イベントハンドリング
+    p {{ counter }}
+    v-btn(small @click="counter++") Click!
+    v-btn(small @click="clickHandler($event, 'Vue.js')") Click!
+    p {{ message }}
+</template>
+
+<script>
+export default {
+  name: 'Sandbox3',
+  data () {
+    return {
+      counter: 0,
+      message: ''
+    }
+  },
+  methods: {
+    clickHandler: function($event, message) {
+      this.counter++
+      this.message = message
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 24px;
+}
+</style>
